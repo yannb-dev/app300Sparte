6b71c48d9b0f5f567d330403e920b2daa880a9b2
yannb-dev <yannblondeaudev@gmail.com>
2026-02-08 15:38:42 +0100

    Animation signUp&logIn sur page accueil

---
+++ index.js
@@ -0,0 +1,24 @@
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogIn = document.getElementById('btnLogIn'); 
+const boite = document.getElementById('slide');
+
+console.log(btnSignUp, btnLogIn, boite);
+
+btnSignUp.addEventListener('click',()=>{
+
+    boite.classList.remove('slideDown');
+    boite.classList.add('slideUp');
+    
+
+    console.log('Slide vers signUp !');
+    
+})
+btnLogIn.addEventListener('click',()=>{
+
+    boite.classList.remove('slideUp');
+    boite.classList.add('slideDown');
+    console.log('Slide vers logIn !');
+    
+})
+
+
