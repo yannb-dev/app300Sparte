bd599165776331be5f145bab44ffdb6a85d4d223
yannb-dev <yannblondeaudev@gmail.com>
2026-02-18 13:25:44 +0100

    Ajout Slide ContainerTraining

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@
 const btnToogle = document.getElementById('darkMode');
 const bulleToogle = document.getElementById('bulle');
 
+console.log('Element du darkMode', btnToogle, bulleToogle)
+
 // VARIANTES PROGRESSION BARRE ---------------------------
 
 const circleMessager = document.getElementById('progress-circleMessager');
@@ -10,6 +12,8 @@ const circleWarrior = document.getElementById('progress-circleWarrior');
 const circleStronger = document.getElementById('progress-circleStronger');
 const radius = 70;
 const circumference = 2 * Math.PI * radius; // 565.48
+
+console.log('Element des badges', circleMessager, circleStronger, circleWarrior, circumference);
 
 // VARIANTES PROGRESSION BARRE ---------------------------
 
@@ -20,6 +24,15 @@ const circumference = 2 * Math.PI * radius; // 565.48
 const containerDate = document.querySelectorAll('.jour');
 const dateToday = new Date();
 
+console.log('Element du calendrier', containerDate, dateToday)
+
+// VARIANTE SLIDECONTAINER ------------------------------
+
+const slideLeft = document.getElementById('iconLeft');
+const slideRight = document.getElementById('iconRight');
+const containerSlide = document.getElementById('slideContainer');
+
+console.log('Element du SLIDECONTAINER', slideLeft, slideRight, containerSlide)
 
 // ======================================================
 // DARKMODE
@@ -94,3 +107,41 @@ containerDate.forEach((boite, index) => {
 
   boite.innerHTML = dayName;
 })
+
+// ======================================================
+// SLIDE DU PLANNING
+// ======================================================
+let position = 0;
+const limiteGauche = 0;
+const limiteDroite = -200;
+const pas = 100;
+
+slideLeft.addEventListener('click', () => {
+  
+  if (position === limiteGauche) {
+    console.log('Slide en butée gauche !')
+  } else {
+    position += pas;
+
+    containerSlide.style.transform = `translateX(${position}%)`;
+    console.log('Clik a gauche !', position);
+  }
+
+ 
+
+})
+
+slideRight.addEventListener('click', () => {
+  
+  if (position === limiteDroite) {
+    console.log('Slide en butée droite !');
+  } else {
+    position -= pas;
+
+    containerSlide.style.transform = `translateX(${position}%)`;
+    console.log('Clik a gauche !', position);
+  }
+ 
+
+})
+
